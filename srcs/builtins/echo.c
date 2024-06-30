05674484e384873524bda2216e50507567d4003e
Ryuuku777 <gateau.nils@gmail.com>
2024-06-30 15:54:40 +0200

    builtins echo export env unset

---
+++ srcs/builtins/echo.c
@@ -0,0 +1,170 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nigateau <nigateau@student.42.lausanne>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/21 18:02:14 by nigateau          #+#    #+#             */
+/*   Updated: 2024/06/27 16:39:16 by nigateau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void    echo(t_token **token)
+{
+    int     i;
+    t_token *curr;
+
+    curr = *token;
+    i = 0;
+    while (curr != NULL)
+    {
+        if (strcmp(curr->command, "echo") == 0)
+            break;
+        curr = curr->next;
+    }
+    //if (strcmp(curr->argument[0], "-n") == 0)
+    //    echo_n(token);
+    while (curr->argument[i] != NULL)
+    {
+        printf("%s", curr->argument[i]);
+        i++;
+    }   
+}
+
+int     size_var_key(char *str)
+{
+    int     i;
+    int     size;
+
+    i = 0;
+    size = 0;
+    while (str[i] != '$')
+        i++;
+    i++;
+    while (str[i] != ' ' && str[i] != '\0')
+    {
+        size++;
+        i++;
+    }
+    return (size);
+}
+
+int     size_var_value(char *str)
+{
+	int	i;
+    int size;
+	char *temp;
+    char *ptr;
+
+	i = 0;
+    size = size_var_key(str);
+    ptr = strdup(str);
+	while (ptr[i] != '$')
+		i++;
+    i++;
+    ptr[i + size] = '\0';
+	temp = getenv(ptr + i);
+	if (temp == NULL)
+		perror("variable not found");
+    strlen(temp);
+    free (ptr);
+	return (strlen(temp));
+}
+
+char    *get_value(char *str)
+{
+	int	i;
+    int size;
+    char *ptr;
+	char *temp;
+    char *var;
+
+	i = 0;
+    ptr = strdup(str);
+    size = size_var_key(ptr);
+	while (ptr[i] != '$')
+		i++;
+    i++;
+    ptr[i + size] = '\0';
+	temp = getenv(ptr + i);
+	if (temp == NULL)
+		perror("variable not found");
+    var = strdup(temp);
+    free(ptr);
+	return (var);
+}
+
+// char *get_value(char *str)
+// {
+//     int i;
+//     int size;
+//     char *ptr;
+//     char *temp;
+//     char *var;
+
+//     i = 0;
+//     ptr = strdup(str);
+//     size = size_var_key(ptr);
+//     char *key = ptr; // Store the original pointer in a separate variable
+//     while (*ptr != '$')
+//         ptr++;
+//     ptr++;
+//     ptr[size] = '\0';
+//     temp = getenv(ptr);
+//     if (temp == NULL)
+//         perror("variable not found");
+//     var = strdup(temp);
+//     free(key); // Free the original pointer
+//     return var;
+// }
+
+char    *expandz(char *str)
+{
+    int     i;
+    int     j;
+    int     size_var;
+    char    *new_str;
+    char    *value;
+
+    i = 0;
+    j = 0;
+    value = get_value(str);
+    size_var = size_var_key(str);
+    new_str = (char *)malloc((sizeof(char) * strlen(str)) -size_var_key(str) +size_var_value(str));
+    if(!new_str)
+        return (NULL);
+    while (str[i] != '$')
+    {
+        new_str[i] = str[i];
+        i++;
+    }
+    while (value[j] != '\0')
+    {
+        new_str[i + j] = value[j];
+        j++;
+    }
+    while (str[i + size_var] != '\0')
+    {
+        new_str[i + j] = str[i + size_var + 1];
+        i++;
+    }
+    new_str[i + j] = '\0';
+    free(str);
+    free (value);
+    return (new_str);
+}
+
+int main()
+{
+    char *str;
+
+    str = strdup("bonjour je suis $USER !");
+    str = expandz(str);
+
+    printf("%s\n", str);
+    free(str);
+    return (1);
+}
--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -3,27 +3,65 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: racinedelarbre <racinedelarbre@student.    +#+  +:+       +#+        */
+/*   By: nigateau <nigateau@student.42.lausanne>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 17:08:17 by racinedelar       #+#    #+#             */
-/*   Updated: 2024/05/28 17:11:35 by racinedelar      ###   ########.fr       */
+/*   Updated: 2024/06/21 17:04:48 by nigateau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#include "../../includes/minishell.h"
 
-char **copy_tab(char **tab)
+
+char    *return_value_quoted(char *str)
 {
-    int i;
-    char **result;
+    int     i;
+    int     size;
+    char    *tmp;
+    char    *value;
 
     i = 0;
-    while (tab[i++])
-        ;
-    result = malloc(sizeof(char *) * i);
-    i = -1;
-    while (tab[++i])
-        result[i] = ft_strdup(tab[i]);
-    result[i] = NULL;
-    return (result);
+    if (!str)
+        return (NULL);
+    tmp = strchr(str, '=');
+    tmp++;
+    tmp++;
+    value = strdup(tmp);
+    size = strlen(value);
+    value[size - 1] = '\0';
+    return (value);
 }
+
+void    export(t_envp **root, char *new_var)
+{
+    int     i;
+    char    *key;
+    char    *value;
+
+	i = 0;
+	while (new_var[i] != '=')
+		i++;
+	if (new_var[++i] == '\"')
+		value = return_value_quoted(new_var);
+    else
+		value = return_value(new_var);
+	key = return_key(new_var);
+    insert_node_env(root, key, value);
+    return;
+}
+
+// char **copy_tab(char **tab)
+// {
+//     int i;
+//     char **result;
+
+//     i = 0;
+//     while (tab[i++])
+//         ;
+//     result = malloc(sizeof(char *) * i);
+//     i = -1;
+//     while (tab[++i])
+//         result[i] = ft_strdup(tab[i]);
+//     result[i] = NULL;
+//     return (result);
+// }
